c01e72233d53f6cdd591db8693f94bc8210b8990
noisonnoiton <noisonnoiton@gmail.com>
2023-05-03 09:54:14 +0900

    remove unused api

--- src/app/api/edge/version/latest-mac.yml/route.ts
+++ src/app/api/edge/version/latest-mac.yml/route.ts
@@ -1,18 +1,8 @@
 import * as superagent from 'superagent';
 
 export async function GET(request: Request) {
-  const isServer = typeof window === "undefined";
-
   const EDGE_APP_URL = process.env.NEXT_PUBLIC_EDGE_APP_URL as string
-  
-  // const res = await (await fetch(EDGE_APP_URL)).json()
-  // let res;
-  // if (!isServer) {
-  //   res = await (await fetch(EDGE_APP_URL)).json();
-  // } else {
-  //   res = { version: "" };
-  // }
-  
+
   const res = await superagent.get(EDGE_APP_URL);
   const yml = `version: ${res.body.version}`
 
